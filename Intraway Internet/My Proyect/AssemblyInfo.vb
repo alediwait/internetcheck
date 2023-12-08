ce69f3353508b44585a07a0eeba1bab641db80fe
AleDiwaIT <45946482+alediwait@users.noreply.github.com>
2023-12-08 10:20:04 -0300

    Add files via upload

---
+++ Intraway Internet/My Proyect/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Intraway Internet")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Intraway Corp")> 
+<Assembly: AssemblyProduct("Intraway Internet")> 
+<Assembly: AssemblyCopyright("Alejandro Morcillo ©  2024")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("5d107fcd-9310-4943-9507-09f942d5a030")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
