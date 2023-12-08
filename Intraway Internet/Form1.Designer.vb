f89ab21623d617158f910d85a7e55af1cbde77b0
AleDiwaIT <45946482+alediwait@users.noreply.github.com>
2023-12-08 10:16:37 -0300

    Add files via upload

---
+++ Intraway Internet/Form1.Designer.vb
@@ -0,0 +1,172 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.MostrarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SalirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Location = New System.Drawing.Point(9, 22)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(278, 42)
+        Me.Panel1.TabIndex = 0
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Consolas", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.Blue
+        Me.Label3.Location = New System.Drawing.Point(228, 17)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(34, 24)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "OK"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Consolas", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(3, 17)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(106, 24)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Internet"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Lucida Sans", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(276, 58)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(28, 24)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "©"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 2500
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 10000
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Text = "InternetCheck"
+        Me.NotifyIcon1.Visible = True
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MostrarToolStripMenuItem, Me.SalirToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(116, 48)
+        '
+        'MostrarToolStripMenuItem
+        '
+        Me.MostrarToolStripMenuItem.Name = "MostrarToolStripMenuItem"
+        Me.MostrarToolStripMenuItem.Size = New System.Drawing.Size(115, 22)
+        Me.MostrarToolStripMenuItem.Text = "Mostrar"
+        '
+        'SalirToolStripMenuItem
+        '
+        Me.SalirToolStripMenuItem.Name = "SalirToolStripMenuItem"
+        Me.SalirToolStripMenuItem.Size = New System.Drawing.Size(115, 22)
+        Me.SalirToolStripMenuItem.Text = "Salir"
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Ivory
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Button1.Location = New System.Drawing.Point(271, 0)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(28, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "-"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(0, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(39, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Label4"
+        Me.Label4.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
+        Me.ClientSize = New System.Drawing.Size(299, 77)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Opacity = 0.7R
+        Me.Text = "PingPong"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Timer2 As System.Windows.Forms.Timer
+    Friend WithEvents NotifyIcon1 As System.Windows.Forms.NotifyIcon
+    Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents MostrarToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents SalirToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+
+End Class
---
+++ Intraway Internet/Form1.vb
@@ -0,0 +1,199 @@
+﻿Public Class Form1
+    Dim check1 = 0
+    Dim intermitencia = 0
+    Dim estado = 0
+    Dim ip = "8.8.8.8"
+    Dim timeout = 1000
+    Dim sw = New Stopwatch()
+    Private Sub Label2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label2.Click
+        MsgBox("Se aceptan donaciones en BTC o bandeja paisa")
+    End Sub
+
+
+
+   
+
+
+
+
+
+
+
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.Icon = My.Resources.iw
+
+
+
+        Label1.ForeColor = Color.ForestGreen
+        Timer1.Enabled = True
+        ContextMenuStrip1.Visible = False
+        NotifyIcon1.Icon = Me.Icon
+        NotifyIcon1.ContextMenuStrip = ContextMenuStrip1
+
+
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+
+
+        Try
+            Dim ping As Long = -1
+            sw.Start()
+            If My.Computer.Network.Ping(ip, timeout) Then
+                sw.Stop()
+                ping = sw.ElapsedMilliseconds
+
+            End If
+            If ping < 0 Then
+                Label1.ForeColor = Color.DarkRed
+                Label3.Show()
+
+                Label1.Text = "Intentos Fallidos: "
+                check1 = check1 + 1
+                Label3.Text = check1
+            ElseIf ping > 0 Then
+                Label1.ForeColor = Color.ForestGreen
+                Label3.Show()
+
+                Label1.Text = "Internet"
+                Label3.Text = "OK"
+                check1 = 0
+            Else
+
+            End If
+        Catch ex As Exception
+            Label4.Text = ex.Message
+            Label4.BackColor = Color.Red
+            Console.WriteLine(ex.ToString())
+        End Try
+
+        If check1 = 9 Then
+            My.Computer.Audio.Play(My.Resources.alarma, AudioPlayMode.Background)
+            Label3.Hide()
+            Label1.Text = "Sin Internet :("
+            NotifyIcon1.ShowBalloonTip(5, "ATENCION", "No hay Internet", ToolTipIcon.Warning)
+            intermitencia = 0
+            Timer1.Enabled = False
+            Timer2.Enabled = True
+
+
+        End If
+
+        If check1 = 4 Then
+            intermitencia = intermitencia + 1
+        End If
+        If intermitencia = 7 Then
+            Label1.Text = "Internet Inestable"
+            MsgBox("Mucha intermitencia", MsgBoxStyle.Critical)
+            Label3.Hide()
+            intermitencia = 0
+
+        End If
+    End Sub
+
+    Private Sub Timer2_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer2.Tick
+        Try
+            Dim ping As Long = -1
+            sw.Start()
+            If My.Computer.Network.Ping(ip, timeout) Then
+                sw.Stop()
+                ping = sw.ElapsedMilliseconds
+
+            End If
+            If ping < 0 Then
+                Label1.ForeColor = Color.DarkRed
+                Label3.Show()
+
+                Label1.Text = "Intentos Fallidos: "
+                check1 = check1 + 1
+                Label3.Text = check1
+            ElseIf ping > 0 Then
+                Label1.ForeColor = Color.ForestGreen
+                Label3.Show()
+                My.Computer.Audio.PlaySystemSound(System.Media.SystemSounds.Asterisk)
+                Label1.Text = "Internet"
+                Label3.Text = "OK"
+                check1 = 0
+                Timer1.Enabled = True
+                Timer2.Enabled = False
+            Else
+
+            End If
+        Catch ex As Exception
+            Label4.Text = ex.Message
+            Label4.BackColor = Color.Red
+            Console.WriteLine(ex.ToString())
+        End Try
+        If check1 = 20 Then
+            My.Computer.Audio.Play(My.Resources.alarma, AudioPlayMode.Background)
+            NotifyIcon1.ShowBalloonTip(5, "ATENCION", "No hay Internet", ToolTipIcon.Warning)
+        End If
+        If check1 = 30 Then
+            My.Computer.Audio.Play(My.Resources.alarma, AudioPlayMode.Background)
+            NotifyIcon1.ShowBalloonTip(5, "ATENCION", "No hay Internet", ToolTipIcon.Warning)
+        End If
+        If check1 = 40 Then
+            My.Computer.Audio.Play(My.Resources.alarma, AudioPlayMode.Background)
+            NotifyIcon1.ShowBalloonTip(5, "ATENCION", "No hay Internet", ToolTipIcon.Warning)
+        End If
+        If check1 = 50 Then
+            My.Computer.Audio.Play(My.Resources.alarma, AudioPlayMode.Background)
+            NotifyIcon1.ShowBalloonTip(5, "ATENCION", "No hay Internet", ToolTipIcon.Warning)
+        End If
+        If check1 = 60 Then
+            My.Computer.Audio.Play(My.Resources.alarma, AudioPlayMode.Background)
+            NotifyIcon1.ShowBalloonTip(5, "ATENCION", "No hay Internet", ToolTipIcon.Warning)
+        End If
+
+
+    End Sub
+
+    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
+
+        estado = estado + 1
+        If estado = 2 Then
+            Label1.ForeColor = Color.ForestGreen
+            Label3.Hide()
+            Label1.Text = "Sistema Activado"
+            estado = 0
+            Timer1.Enabled = True
+
+        Else
+            Label1.ForeColor = Color.OrangeRed
+            Label3.Hide()
+            Label1.Text = "Sistema Desactivado"
+            Timer1.Enabled = False
+            Timer2.Enabled = False
+
+        End If
+        
+
+    End Sub
+
+    Private Sub SalirToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SalirToolStripMenuItem.Click
+
+        Me.Close()
+
+
+
+    End Sub
+
+    Private Sub MostrarToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MostrarToolStripMenuItem.Click
+        Me.Show()
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Me.Hide()
+
+    End Sub
+
+    Private Sub NotifyIcon1_MouseDoubleClick(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles NotifyIcon1.MouseDoubleClick
+        Me.Show()
+
+    End Sub
+
+    
+   
+End Class
